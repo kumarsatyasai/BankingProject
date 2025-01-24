0d535c43fe7ab0f230b86877b44df082176c53cf
Kumar Satya Sai <tulasikumarkande@gmail.com>
2025-01-24 13:14:17 +0530

    Transaction Details commit.

--- Project44-BankingApplication-1/src/main/java/com/nt/service/EmailServiceMgmtImpl.java
+++ Project44-BankingApplication-1/src/main/java/com/nt/service/EmailServiceMgmtImpl.java
@@ -1,6 +1,6 @@
 package com.nt.service;
 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
